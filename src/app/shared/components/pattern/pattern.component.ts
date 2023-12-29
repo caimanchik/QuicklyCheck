afe577893e8d274cb63abb81dd694e4b054cba2d
caiman <shimyshimya@mail.ru>
2023-12-29 18:07:02 +0500

    рефакторинг бланков

--- src/app/shared/components/pattern/pattern.component.ts
+++ src/app/shared/components/pattern/pattern.component.ts
@@ -1,24 +1,79 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { AfterViewChecked, ChangeDetectionStrategy, Component, EventEmitter, Input, Output } from '@angular/core';
 import { PatternParsed } from "../../interfaces/Tests/Patterns/PatternParsed";
+import { transition, trigger, useAnimation } from "@angular/animations";
+import { transformOpacity } from "../../animations/transform-opacity";
 
 @Component({
   selector: 'app-pattern',
   templateUrl: './pattern.component.html',
-  styleUrls: ['./pattern.component.scss']
+  styleUrls: ['./pattern.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('appear', [
+      transition(':enter',
+        useAnimation(transformOpacity), {
+          params: {
+            oStart: 0,
+            oEnd: 1,
+            transformStart: "translateY(10px)",
+            transformEnd: "translateY(0px)",
+          }
+        }),
+    ])
+  ],
 })
-export class PatternComponent {
+export class PatternComponent implements AfterViewChecked {
   @Input() public patterns!: PatternParsed[]
   @Output() public patternChanges = new EventEmitter<PatternParsed>()
+  @Output() public nextClicked = new EventEmitter<void>()
 
   protected selected = 0
-
-  protected varIs = [1, 2, 3, 4, 5, 6, 7, 8]
+  protected filled = [false, false, false, false, false, false, false, false]
 
   constructor() { }
 
-  protected readonly Array = Array;
+  ngAfterViewChecked(): void {
+    this.updateFilling()
+  }
 
   protected changePattern(varI: number) {
     this.selected = varI - 1
+
+    if (this.patterns.length < varI) {
+      for (let i = this.patterns.length; i < varI; i++) {
+        this.patterns.push({
+          pattern: Array<number>(40).fill(-1),
+          num: i + 1,
+          test: 3,
+          pk: 2
+        })
+      }
+    }
+
+    this.updateFilling()
+  }
+
+  protected addAnswer(question: number, value: number) {
+    if (question > 0 && this.patterns[this.selected].pattern[question - 1] === -1)
+      return
+
+    if (this.patterns[this.selected].pattern[question] === value
+      && (question != 39 && this.patterns[this.selected].pattern[question + 1] === -1 || question === 39)
+    )
+      value = -1
+
+    this.patterns[this.selected].pattern[question] = value
+
+    this.patternChanges.next(this.patterns[this.selected]);
+    this.updateFilling()
+  }
+
+  private updateFilling() {
+    if (this.patterns)
+      this.filled = this.patterns.map(e => e.pattern[0] !== -1)
+  }
+
+  protected clickButton() {
+    this.nextClicked.next()
   }
 }
