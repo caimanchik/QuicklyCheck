e8e7e4bfc21cbce6a53690ba5756ee0dd986f5a6
caiman <shimyshimya@mail.ru>
2025-01-22 02:49:28 +0500

    рефакторинг

--- src/app/shared/interfaces/Views/IBlankValidView.ts
+++
@@ -1,6 +0,0 @@
-import { IBlankView } from "./IBlankView";
-import { IBlankValid } from "../Tests/Blanks/IBlankValid";
-
-export interface IBlankValidView extends Omit<IBlankView, 'blank'> {
-  blank: IBlankValid
-}
